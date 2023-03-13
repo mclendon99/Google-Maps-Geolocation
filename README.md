cab1e5ed27ccfed0e0492164de5bd916e99e0696
mclendon99 <postmasters@bellsouth.net>
2023-03-13 15:43:57 -0400

    CSP still needs work, but it runs now.
    Separated .css, .html, and .js files
    Updated README.

---
+++ README.md
@@ -0,0 +1,20 @@
+### Windows Map Embed with your location pinned
+
+This is a collection of front-end javascript, css and html to display your current location.
+The display is a Google Map. No google key is required. Tested on Windows 10 (Edge) but should run on Windows 11.
+The app prompts for permission to access location as it request a high accuracy location.
+
+The files are organized as follows:
+
+1. index.html, index.js, index.css (Geolocation page)
+Collect the necessary geolocation information using the navigator.geolocation.getCurrentPosition API.
+No cookies or local storage is used - ever.
+
+The user MUST enable Settings -> Privacy -> Location -> Allow desktop apps to access your location
+or the position shown will be very wrong.
+
+
+TODO 
+- Get the PITA CSP policy stuff running.
+- Add in an automatic refresh/update slider type timer. For now, refresh to update the map.
+- Package as a windows store app (manifest file).
---
+++ index.js
@@ -0,0 +1,36 @@
+
+var pos = null;
+var x = document.getElementById("body");
+
+function showPosition(p)
+{
+    pos = p;
+    console.log("Position:" + pos.coords.latitude + ',' + pos.coords.longitude);
+
+
+    const contentStr = '<div class="mapouter"><div class="gmap_canvas"> ' +
+      '<iframe class="gmap_iframe" referrerpolicy="no-referrer-when-downgrade" sandbox="allow-same-origin allow-presentation allow-scripts" ' +
+      'src="https://maps.google.com/maps?hl=en&q=' + pos.coords.latitude + ',' + pos.coords.longitude +
+      '&z=18&ie=UTF8&iwloc=B&output=svembed"></iframe></div></div>';
+    console.log("contentStr:", contentStr);
+    x.innerHTML = contentStr;
+}
+
+function errorPosition(e) 
+{
+    console.log("Position error:" + e);
+    x.innerHTML = "Geolocation is not supported by this browser.";
+}
+
+async function getPosition()
+{
+    if (navigator.geolocation) {
+        const w = await navigator.geolocation.getCurrentPosition(showPosition, errorPosition , { enableHighAccuracy: true });
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+
+getPosition();
